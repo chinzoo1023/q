999657d33cdfadf13f07dce4a78976d694ff16f1
chinzoo1023 <famebatzo@gmail.com>
2024-10-20 17:13:43 +0800

    q

---
+++ Haaland.js
@@ -0,0 +1,25 @@
+// console.log() -> hevleh Function
+console.log("hello world");
+console.log(5*8/100+666);
+console.log("hi"+3);
+console.log("5+5="+(5+5));
+// variable -> huvisagch
+var a=5;
+console.log(a+19999995);
+var name = "RONALDO", age=37;
+console.log("my name is " + name+"I am "+age+" years old.");
+console.log("2+2="+(2+3));
+var urt=9, urgun=7;
+console.log("s:"+(urt*urgun));
+console.log("p:"+((urt+urgun)*2));
+// promt() -> garaas utga oruulah
+var ner = prompt("neree oruulah");
+alert("Tanii oruulsan ner "+ner);
+var nas = prompt("nasaa oruulna uu");
+alert(2024-nas);
+var too=+prompt("2 orontoi too oruulna uu");
+var neg=Math.floor(too/10) ;
+console.log("Ehnii oron:"+neg)
+var hoyr=too%10;
+alert(neg+hoyr);
+var +prompt("hussen seckut bich")
